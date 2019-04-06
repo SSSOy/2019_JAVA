f44f31ea8f9d9d2f684ce0689d20e4007e561704
SSSO <thdud00627@gmail.com>
2019-04-06 23:40:18 +0900

    first commit

---
+++ 190404_Test2/src/test1.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class test1 {
+	public static void main(String[] args) {
+		Frame f = new Frame("190404 ù AWT");
+		f.setBounds(500, 300, 300, 300);
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdepter());
+	}
+}
